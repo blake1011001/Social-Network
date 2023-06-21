dc8d38f82e1d54433e811a952fcb6448825741ef
Blake Sanders <blakesand00@gmail.com>
2023-06-20 21:03:07 -0400

    finished setting up routes

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,17 @@
+const router = require('express').Router();
+
+const {
+    getThought,
+    getSingleThought,
+    createThought,
+    updateThought,
+    deleteThought,
+    addReaction,
+    removeReaction
+} = require('../../controllers/thought-controller');
+router.route('/').get(getThought).post(createThought);
+router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
+router.route('/:thoughtId/reaction').post(addReaction);
+router.route('/:thoughtId/reaction/:reaactionId').delete(removeReaction);
+
+module.exports = router;
